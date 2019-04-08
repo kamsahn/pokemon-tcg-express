5d5ccadefcb4aae38745f99b60ed8dad9bf23a87
kamsahn <samjkahn@gmail.com>
2019-04-08 16:39:27 -0400

    Create deck model and routes
    - remove ownership for testing

---
+++ app/routes/deck_routes.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const passport = require('passport')
+
+const Deck = require('../models/deck')
+
+const customErrors = require('../../lib/custom_errors')
+const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+const removeBlanks = require('../../lib/remove_blank_fields')
+const requireToken = passport.authenticate('bearer', { session: false })
+
+const router = express.Router()
+
+// to require ownership, add requireToken as second argument in router functions
+router.get('/decks', (req, res, next) => {
+  Deck.find()
+    .then(decks => {
+      return decks.map(deck => deck.toObject())
+    })
+    .then(decks => res.status(200).json({ decks }))
+    .catch(next)
+})
+
+router.get('/decks/:id', (req, res, next) => {
+  Deck.findById(req.params.id)
+    .then(handle404)
+    .then(deck => res.status(200).json({ deck: deck.toObject() }))
+    .catch(next)
+})
+
+router.post('/decks', requireToken, (req, res, next) => {
+  req.body.deck.owner = req.user.id
+
+  Deck.create(req.body.deck)
+    .then(deck => {
+      res.status(201).json({ deck: deck.toObject() })
+    })
+    .catch(next)
+})
+
+router.patch('/decks/:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.deck.owner
+
+  Deck.findById(req.params.id)
+    .then(handle404)
+    .then(deck => {
+      requireOwnership(req, deck)
+
+      return deck.update(req.body.deck)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+router.delete('/decks/:id', requireToken, (req, res, next) => {
+  Deck.findById(req.params.id)
+    .then(handle404)
+    .then(deck => {
+      requireOwnership(req, deck)
+      deck.remove()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
+const deckRoutes = require('./app/routes/deck_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require error handling middleware
@@ -59,6 +60,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // register route files
 app.use(exampleRoutes)
+app.use(deckRoutes)
 app.use(userRoutes)
 
 // register error handling middleware
